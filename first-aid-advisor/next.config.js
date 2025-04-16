5aa22e6b579e1c2b377b651bcac8c4f895976789
Samiabdus786 <samiabdus9831@gmail.com>
2025-04-16 18:13:39 +0530

    Updated

--- first-aid-advisor/next.config.js
+++ first-aid-advisor/next.config.js
@@ -17,13 +17,13 @@ const nextConfig = {
     domains: ['www.google.com', 'google.com'],
   },
   env: {
-    // Gemini
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    
-    // Pinecone
     PINECONE_API_KEY: process.env.PINECONE_API_KEY,
     PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT,
     PINECONE_INDEX: process.env.PINECONE_INDEX,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
